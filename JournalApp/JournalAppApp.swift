7e8e036f2bcf87f3197becf487194c5b2af81ce8
faiyamm <sofia.almaraz@uabc.edu.mx>
2026-02-13 19:48:00 -0800

    Initial Commit

---
+++ JournalApp/JournalAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  JournalAppApp.swift
+//  JournalApp
+//
+//  Created by Fai on 13/02/26.
+//
+
+import SwiftUI
+
+@main
+struct JournalAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
